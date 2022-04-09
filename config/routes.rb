c721ef1340b20e5076e32cc73b6b59fdb360ec46
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-09 12:34:55 -0400

    view task added

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 1. read all tasks
   root to: 'tasks#index'
+  get 'tasks/index', to: 'tasks#index', as: 'index'
   # 2. read one task
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   # 3. create a task
   post 'tasks', to: 'tasks#create'
   # 4. update a task
